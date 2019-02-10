43ac2d8e58118ec75f20bdee20fc286626b36aae
iwasawa15 <tatsuya57sak@gmail.com>
2019-02-10 04:23:18 +0000

    api controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,13 @@ class ProductsController < RankingController
   end
 
   def search
-    # @products = Product.where('title LIKE(?)', "%#{params[:keyword]}%").limit(20)
-    keyword = "%#{params[:keyword]}%"
-    @products = Product.find_by_sql(["select * from products where title like ? LIMIT 20", keyword])
+    @products = Product.where('title LIKE(?)', "%#{params[:keyword]}%").limit(20)
+    # keyword = "%#{params[:keyword]}%"
+    # @products = Product.find_by_sql(["select * from products where title like ? LIMIT 20", keyword])
+    
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 end
